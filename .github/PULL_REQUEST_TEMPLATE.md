867448bcf41e448d4fb724a54ff0276c92a71876
Willem Olding <willemolding@gmail.com>
2021-05-19 12:29:17 +1000

    initialize repo!

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,18 @@
+**Summary of changes**
+Changes introduced in this pull request:
+- 
+
+
+
+**Reference issue to close (if applicable)**
+<!-- Include the issue reference this pull request is connected to -->
+<!--(e.g. Closes #1)-->
+Closes 
+
+
+**Other information and links**
+<!-- Add any other context about the pull request here. -->
+
+
+
+<!-- Thank you 🔥 -->
---
+++ scripts/add_license.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PAT_APA="^// Copyright 2020 ChainSafe Systems // SPDX-License-Identifier: Apache-2.0$"
+
+valid=true
+for file in $(find . -type f -not -path "./target/*" -not -path "./blockchain/beacon/src/drand_api/*" -not -path "./ipld/graphsync/src/message/proto/message.rs" | egrep '\.(rs)$'); do
+	header=$(echo $(head -3 $file))
+	if ! echo "$header" | grep -q "$PAT_APA"; then
+		echo "$file was missing header"
+		cat ./scripts/copyright.txt $file > temp
+		mv temp $file
+		valid=false
+	fi
+done
+
+# if a header is incorrect, return an OS exit code
+if [ "$valid" = false ] ; then
+	exit 1
+fi
