7193ae6d0a2244bed683f1bc43d6bf0fe7c15c70
Willem Olding <willemolding@gmail.com>
2021-06-08 16:51:32 +1000

    changes base58 trait to use serialize impl. Adds base58 serializing for keys

--- crypto/src/keys.rs
+++ crypto/src/keys.rs
@@ -1,3 +1,4 @@
+use crate::base_58::MinaBase58;
 use serde::{Deserialize, Serialize};
 use serde_versions_derive::version;
 
@@ -25,10 +26,17 @@ impl PublicKey {
     }
 }
 
+impl MinaBase58 for PublicKey {
+    fn version_byte() -> u8 {
+        crate::base58_version_bytes::NON_ZERO_CURVE_POINT_COMPRESSED
+    }
+}
+
 #[cfg(test)]
 pub mod tests {
 
     use super::PublicKey;
+    use crate::base_58::MinaBase58;
     use serde_bin_prot::to_writer;
 
     #[test]
@@ -36,5 +44,12 @@ pub mod tests {
         let mut buf = Vec::new();
         to_writer(&mut buf, &PublicKey::new()).unwrap();
         println!("{:?}", buf)
+    }
+
+    #[test]
+    fn from_base58_roundtrip() {
+        let s = "B62qonDZEKYULNkfq7WGu1Z881YBRnMSuBGGX5DhnTv26mUyvN99mpo";
+        let k = PublicKey::from_base58(s).unwrap();
+        assert_eq!(s, k.to_base58().into_string())
     }
 }
