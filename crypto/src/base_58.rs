61a18fc68ee5477017169b2fb3090f14d19c414d
Willem Olding <willemolding@gmail.com>
2021-06-08 16:55:14 +1000

    fmt and fix clippy

--- crypto/src/base_58.rs
+++ crypto/src/base_58.rs
@@ -1,22 +1,22 @@
-use serde::{Deserialize, Serialize};
-use serde_bin_prot::{to_writer, from_reader};
 use bs58::encode::EncodeBuilder;
+use serde::{Deserialize, Serialize};
+use serde_bin_prot::{from_reader, to_writer};
 
 pub use bs58::decode::Error;
 pub use bs58::{decode, encode};
 
 pub trait MinaBase58 {
     /// This is the only method a custom implementation need provide.
-    /// Should be a constant from the version_bytes.rs file corresponding
+    /// Should be a constant from the base58_version_bytes.rs file corresponding
     /// to the type.
     fn version_byte() -> u8;
 
-    fn to_base58(self) -> EncodeBuilder<'static, Vec<u8>>
+    fn to_base58(&self) -> EncodeBuilder<'static, Vec<u8>>
     where
         Self: Sized + Serialize,
     {
         let mut buf = Vec::<u8>::new();
-        to_writer(&mut buf, &self).unwrap();
+        to_writer(&mut buf, self).unwrap();
         encode(buf).with_check_version(Self::version_byte())
     }
 
@@ -28,7 +28,7 @@ pub trait MinaBase58 {
         let bytes: Vec<u8> = decode(i)
             .with_check(Some(Self::version_byte()))
             .into_vec()?;
-            
+
         // skip the first byte as this still contains the version byte
         Ok(from_reader(&bytes[1..]).unwrap())
     }
--- crypto/src/keys.rs
+++ crypto/src/keys.rs
@@ -3,27 +3,16 @@ use serde::{Deserialize, Serialize};
 use serde_versions_derive::version;
 
 #[version(1)]
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Default, Clone, Serialize, Deserialize)]
 pub struct CompressedPoly {
     x: [u8; 32],
     is_odd: bool,
 }
 
 #[version(1)]
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Default, Clone, Serialize, Deserialize)]
 pub struct PublicKey {
     poly: CompressedPoly,
-}
-
-impl PublicKey {
-    pub fn new() -> Self {
-        PublicKey {
-            poly: CompressedPoly {
-                x: [0x0; 32],
-                is_odd: false,
-            },
-        }
-    }
 }
 
 impl MinaBase58 for PublicKey {
@@ -42,7 +31,7 @@ pub mod tests {
     #[test]
     fn serialize_empty_keypair() {
         let mut buf = Vec::new();
-        to_writer(&mut buf, &PublicKey::new()).unwrap();
+        to_writer(&mut buf, &PublicKey::default()).unwrap();
         println!("{:?}", buf)
     }
 
