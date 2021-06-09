4c7f1304d158863166d0990857d802a2ffecd10e
Willem Olding <willemolding@gmail.com>
2021-06-09 16:26:15 +1000

    adds MinaHash trait that is generic over hash algo

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub mod base_58;
-pub mod hash_types;
+pub mod hash;
 pub mod hash_prefixes;
+pub mod hash_types;
 pub mod keys;
 
 pub mod base58_version_bytes;
