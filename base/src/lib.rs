28ed5fe3b13b1e44bdb10b8135052c5067188edd
Willem Olding <willemolding@gmail.com>
2021-06-09 17:56:11 +1000

    allow legacy derive helpers

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -1,6 +1,13 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0
 
+// Need to supress this warning for the moment so we can use the #[verson(x)]
+// attribute macro. It seems we are in an awkward inbetween phase where it can't be 
+// defined above a derive macro (or the following warning) and it can't be defined 
+// below or it will error. The error will be fixed in the future when derive becomes like a regular
+// attribute macro and an order of operations well defined
+#![allow(legacy_derive_helpers)]
+
 pub mod blockchain_state;
 pub mod consensus_state;
 pub mod epoch_data;
