61cdd00c027ea2e6db1a4010e70e0497e8306a9d
Willem Olding <willemolding@gmail.com>
2021-09-28 21:22:56 +1000

    adds shadow type in implementation that fixes recursion issue

--- wire-type/src/lib.rs
+++ wire-type/src/lib.rs
@@ -6,13 +6,13 @@ use serde::{Deserialize, Serialize};
 
 pub use wire_type_derive::*;
 
-pub type Version = u8;
+pub type Version = u16;
 
 pub trait WireType<'a>:
     Debug + PartialEq + Serialize + Deserialize<'a> + From<Self::WireType>
 {
     type WireType: Serialize + Deserialize<'a>;
-    const VERSION: u8;
+    const VERSION: Version;
 
     fn to_wire_type(self) -> Self::WireType;
     fn from_wire_type(t: Self::WireType) -> Self;
@@ -21,7 +21,7 @@ pub trait WireType<'a>:
 #[cfg(test)]
 mod tests {
     use crate as wire_type;
-    use wire_type::WireType;
+    use wire_type::{WireType, Version};
     use serde::{Deserialize, Serialize};
 
     #[test]
@@ -33,7 +33,7 @@ mod tests {
 
         #[derive(Serialize, Deserialize)]
         struct WireX {
-            version: u8,
+            version: Version,
             inner: X,
         }
 
@@ -45,7 +45,7 @@ mod tests {
 
         impl<'a> WireType<'_> for X {
             type WireType = WireX;
-            const VERSION: u8 = 0x01;
+            const VERSION: Version = 0x01;
             fn to_wire_type(self) -> Self::WireType {
                 Self::WireType {
                     version: Self::VERSION,
@@ -61,7 +61,7 @@ mod tests {
     #[test]
     fn derive() {
         #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, WireType)]
-                struct X {
+        struct X {
             a: u32,
         }
 
@@ -87,12 +87,71 @@ mod tests {
     }
 
     #[test]
-    fn compatible_with_serde_from_and_into() {
+    fn compatible_with_serde_from_and_into_named_struct() {
         #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, WireType)]
-        #[serde(from = "<Self as WireType>::WireType")] // can be deserialized from its wire type
-        #[serde(into = "<Self as WireType>::WireType")] // will be serialized to its wire type
+        #[serde(from = "<Self as WireType>::WireType")]
+        #[serde(into = "<Self as WireType>::WireType")]
         struct X {
             a: u32,
+            b: u32,
         }
+
+        let x = X {a: 123, b: 321};
+
+        let serialized = serde_json::to_string(&x).unwrap();
+        println!("serialized = {}", serialized);
+
+        let deserialized: X = serde_json::from_str(&serialized).unwrap();
+        println!("deserialized = {:?}", deserialized);
+    }
+
+    #[test]
+    fn compatible_with_serde_from_and_into_unnamed_struct() {
+        #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, WireType)]
+        #[serde(from = "<Self as WireType>::WireType")]
+        #[serde(into = "<Self as WireType>::WireType")]
+        struct X(u32, u32);
+
+        let x = X (123, 321);
+
+        let serialized = serde_json::to_string(&x).unwrap();
+        println!("serialized = {}", serialized);
+
+        let deserialized: X = serde_json::from_str(&serialized).unwrap();
+        println!("deserialized = {:?}", deserialized);
+    }
+
+    #[test]
+    fn compatible_with_serde_from_and_into_unit_struct() {
+        #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, WireType)]
+        #[serde(from = "<Self as WireType>::WireType")]
+        #[serde(into = "<Self as WireType>::WireType")]
+        struct X;
+
+        let x = X;
+
+        let serialized = serde_json::to_string(&x).unwrap();
+        println!("serialized = {}", serialized);
+
+        let deserialized: X = serde_json::from_str(&serialized).unwrap();
+        println!("deserialized = {:?}", deserialized);
+    }
+
+    #[test]
+    fn compatible_with_serde_from_and_into_enum() {
+        #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, WireType)]
+        #[serde(from = "<Self as WireType>::WireType")]
+        #[serde(into = "<Self as WireType>::WireType")]
+        enum X {
+            X1(u32),
+        }
+
+        let x = X::X1(123);
+
+        let serialized = serde_json::to_string(&x).unwrap();
+        println!("serialized = {}", serialized);
+
+        let deserialized: X = serde_json::from_str(&serialized).unwrap();
+        println!("deserialized = {:?}", deserialized);
     }
 }
