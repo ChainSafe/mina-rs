d28535e6106d63ee1e2c7c5d21f7888a601e4757
hanabi1224 <harlowmoo@gmail.com>
2021-10-18 14:15:28 +0800

    Impl ProtocolConstants (#58)
    * Impl ProtocolConstants
    
    * Separate test_protocol_state_body_constants fn

--- base/src/numbers.rs
+++ base/src/numbers.rs
@@ -2,9 +2,23 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use serde::{Deserialize, Serialize};
+use wire_type::WireType;
 
-#[derive(Clone, Serialize, Deserialize, PartialEq, PartialOrd, Debug, Hash, Copy, Default)]
+#[derive(
+    Clone, Serialize, Deserialize, PartialEq, PartialOrd, Debug, Hash, Copy, Default, WireType,
+)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+#[wire_type(recurse = 2)]
 pub struct Length(pub u32);
+
+#[derive(
+    Clone, Serialize, Deserialize, PartialEq, PartialOrd, Debug, Hash, Copy, Default, WireType,
+)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+#[wire_type(recurse = 2)]
+pub struct Delta(pub u32);
 
 #[derive(Clone, Serialize, Deserialize, PartialEq, Debug, Hash, Default)]
 pub struct Amount(u64);
@@ -12,7 +26,10 @@ pub struct Amount(u64);
 #[derive(Clone, Serialize, Deserialize, PartialEq, Debug, Hash, Default)]
 pub struct GlobalSlot(pub u32);
 
-#[derive(Clone, Serialize, Deserialize, PartialEq, Debug, Hash, Default)]
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug, Hash, Default, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+#[wire_type(recurse = 2)]
 pub struct BlockTime(u64);
 
 #[derive(Clone, Serialize, Deserialize, PartialEq, Debug, Hash, Default)]
