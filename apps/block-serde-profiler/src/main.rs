0f99e04b83cd58018d4ba2fb5a25f5dc309022d8
hanabi1224 <harlowmoo@gmail.com>
2021-11-15 20:04:58 +0800

    heap / cpu profiler for M1

---
+++ apps/block-serde-profiler/src/main.rs
@@ -0,0 +1,66 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0
+
+use bin_prot::Deserializer;
+use clap::{App, Arg};
+use mina_rs_base::types::ExternalTransition;
+use serde::Deserialize;
+use std::str::FromStr;
+
+const BLOCK_BYTES: &[u8] = include_bytes!("../../../protocol/test-fixtures/src/data/block1");
+
+fn main() -> anyhow::Result<()> {
+    let matches = App::new("block-serde-app")
+        .arg(
+            Arg::with_name("mode")
+                .short("m")
+                .long("mode")
+                .value_name("MODE")
+                .possible_values(&["cpu", "heap"])
+                .required(true)
+                .default_value("cpu")
+                .help("Profiling mode")
+                .takes_value(true),
+        )
+        .get_matches();
+    let mode = Mode::from_str(matches.value_of("mode").unwrap()).unwrap();
+    match mode {
+        Mode::Cpu => cpu_profile_serialization(),
+        Mode::Heap => heap_profile_serialization(),
+    }
+    .unwrap();
+    Ok(())
+}
+
+fn cpu_profile_serialization() -> anyhow::Result<ExternalTransition> {
+    let mut de = Deserializer::from_reader(BLOCK_BYTES);
+    Ok(Deserialize::deserialize(&mut de)?)
+}
+
+fn heap_profile_serialization() -> anyhow::Result<ExternalTransition> {
+    use dhat::{Dhat, DhatAlloc};
+
+    #[global_allocator]
+    static ALLOCATOR: DhatAlloc = DhatAlloc;
+    let _dhat = Dhat::start_heap_profiling();
+
+    cpu_profile_serialization()
+}
+
+#[derive(Debug)]
+enum Mode {
+    Cpu,
+    Heap,
+}
+
+impl FromStr for Mode {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "cpu" => Ok(Mode::Cpu),
+            "heap" => Ok(Mode::Heap),
+            _ => Err("no match"),
+        }
+    }
+}
