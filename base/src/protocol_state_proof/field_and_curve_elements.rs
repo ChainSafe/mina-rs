69570ae72d95050a8ed3d1b9de35558aff82ffc5
Willem Olding <willemolding@gmail.com>
2021-11-17 10:25:37 +1100

    Refactor protocol state proof (#81)
    * replace ProtocolStateProof with wrapper from arkworks and add unimplemented traits
    
    * rename some types in protocol-state-proof to be more descriptive
    
    add conversion from protocol ec type to arkworks
    
    update tests
    
    add conversion for PolyComm from ProffMessages
    
    add conversion for OpeningProof
    
    tidy conversions for vectors of scalar points
    
    refactor protocol_state_proof module
    
    fmt and add license
    
    refactor conversions to live with their types
    
    add comments and rename module for ec and ff points
    
    refactor bulletproof challenges
    
    make some bullet proof challenges public to enable tests
    
    * add missing header and fmt
    
    * reinstate commented out test
    
    * replace into with from
    
    * add simple test for BigInt conversion
    
    * fmt
    
    * add js feature to getrandom to support wasm-unknown-unknown build
    
    * lock revs for github deps
    
    * warn on missing license for now
    
    * also deny on copyleft for now
    
    * add mpl to allowed

---
+++ base/src/protocol_state_proof/field_and_curve_elements.rs
@@ -0,0 +1,132 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0
+
+use serde::{Deserialize, Serialize};
+use wire_type::WireType;
+
+use ark_ec::models::ModelParameters;
+use ark_ec::short_weierstrass_jacobian::GroupAffine;
+
+use crate::numbers::BigInt256;
+
+/// Represents an element in a finite field that can be encoded as
+/// a BigInt256. All finite field elements used in Mina satisfiy this requirement
+pub type FieldElement = BigInt256;
+
+/// Vector of finite field elements (with version number defined in the WireType)
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct FieldElementVec(pub Vec<FieldElement>);
+
+impl<Fs> From<FieldElementVec> for Vec<Fs>
+where
+    Fs: From<ark_ff::BigInteger256>,
+{
+    fn from(t: FieldElementVec) -> Self {
+        t.0.into_iter()
+            .map(|i| ark_ff::BigInteger256::from(i).into())
+            .collect()
+    }
+}
+
+/// An elliptic curve point defined over a base field with elements that fit in a BigInt256
+/// This is a Finite elliptic curve point as this type cannot be used to encode the point-at-infinity
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug)]
+pub struct FiniteECPoint(pub FieldElement, pub FieldElement);
+
+impl<P> From<FiniteECPoint> for GroupAffine<P>
+where
+    P: ark_ec::SWModelParameters,
+    <P as ModelParameters>::BaseField: From<ark_ff::BigInteger256>,
+{
+    fn from(FiniteECPoint(x, y): FiniteECPoint) -> Self {
+        Self::new(
+            ark_ff::BigInteger256::from(x).into(),
+            ark_ff::BigInteger256::from(y).into(),
+            false,
+        )
+    }
+}
+
+/// Vector of finite EC points (with version number defined in the WireType)
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct FiniteECPointVec(Vec<FiniteECPoint>);
+
+impl<P> From<FiniteECPointVec> for Vec<GroupAffine<P>>
+where
+    P: ark_ec::SWModelParameters,
+    <P as ModelParameters>::BaseField: From<ark_ff::BigInteger256>,
+{
+    fn from(v: FiniteECPointVec) -> Self {
+        v.0.into_iter().map(Into::into).collect()
+    }
+}
+
+/// Vector of 2-tuples of finite EC points (with version number defined in the WireType)
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct FiniteECPointPairVec(Vec<(FiniteECPoint, FiniteECPoint)>);
+
+impl<P> From<FiniteECPointPairVec> for Vec<(GroupAffine<P>, GroupAffine<P>)>
+where
+    P: ark_ec::SWModelParameters,
+    <P as ModelParameters>::BaseField: From<ark_ff::BigInteger256>,
+{
+    fn from(v: FiniteECPointPairVec) -> Self {
+        v.0.into_iter().map(|(x, y)| (x.into(), y.into())).collect()
+    }
+}
+
+/// Elliptic curve point that can either be the coordinates of a point on the curve
+/// OR it can be the point-at-infinity
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub enum ECPoint {
+    // elliptic curve point, can be the point at infinity
+    Infinite,
+    Finite(FiniteECPoint),
+}
+
+impl Default for ECPoint {
+    fn default() -> Self {
+        Self::Infinite
+    }
+}
+
+impl<P> From<ECPoint> for GroupAffine<P>
+where
+    P: ark_ec::SWModelParameters,
+    <P as ModelParameters>::BaseField: From<ark_ff::BigInteger256>,
+{
+    fn from(p: ECPoint) -> Self {
+        match p {
+            ECPoint::Infinite => Self::new(Default::default(), Default::default(), true),
+            ECPoint::Finite(FiniteECPoint(x, y)) => Self::new(
+                ark_ff::BigInteger256::from(x).into(),
+                ark_ff::BigInteger256::from(y).into(),
+                false,
+            ),
+        }
+    }
+}
+
+/// Vector of EC points (with version number defined in the WireType)
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct ECPointVec(Vec<ECPoint>);
+
+impl<P> From<ECPointVec> for Vec<GroupAffine<P>>
+where
+    P: ark_ec::SWModelParameters,
+    <P as ModelParameters>::BaseField: From<ark_ff::BigInteger256>,
+{
+    fn from(v: ECPointVec) -> Self {
+        v.0.into_iter().map(Into::into).collect()
+    }
+}
---
+++ base/src/protocol_state_proof/mod.rs
@@ -0,0 +1,144 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0
+
+use serde::{Deserialize, Serialize};
+use wire_type::WireType;
+
+use crate::numbers::{BigInt256, Char, Hex64};
+
+pub mod proof_messages;
+pub use proof_messages::ProofMessages;
+
+pub mod proof_evaluations;
+pub use proof_evaluations::ProofEvaluations;
+
+pub mod opening_proof;
+pub use opening_proof::OpeningProof;
+
+pub mod bulletproof_challenges;
+pub use bulletproof_challenges::{
+    BulletproofChallenge, BulletproofChallengeTuple17, BulletproofChallengeTuple18,
+    BulletproofChallenges, BulletproofPreChallenge, ProofStateBulletproofChallenges,
+    ScalarChallengeVector2,
+};
+
+pub mod field_and_curve_elements;
+pub use field_and_curve_elements::{
+    ECPoint, ECPointVec, FieldElement, FieldElementVec, FiniteECPoint, FiniteECPointPairVec,
+    FiniteECPointVec,
+};
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+#[wire_type(recurse = 4)]
+pub struct ProtocolStateProof {
+    pub statement: ProofStatement,
+    pub prev_evals: PrevEvals,
+    pub prev_x_hat: PrevXHat,
+    pub proof: Proof,
+}
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+#[wire_type(recurse = 2)]
+pub struct ProofStatement {
+    pub proof_state: ProofState,
+    pub pass_through: PairingBased,
+}
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct ProofState {
+    pub deferred_values: ProofStateDeferredValues,
+    pub sponge_digest_before_evaluations: SpongeDigestBeforeEvaluations,
+    pub me_only: ProofStatePairingBased,
+}
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct ProofStateDeferredValues {
+    pub plonk: Plonk,
+    pub combined_inner_product: ShiftedValue,
+    pub b: ShiftedValue,
+    pub xi: BulletproofPreChallenge,
+    pub bulletproof_challenges: BulletproofChallengeTuple18,
+    pub which_branch: Char,
+}
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct Plonk {
+    pub alpha: BulletproofPreChallenge,
+    pub beta: ScalarChallengeVector2,
+    pub gamma: ScalarChallengeVector2,
+    pub zeta: BulletproofPreChallenge,
+}
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+#[non_exhaustive]
+pub enum ShiftedValue {
+    ShiftedValue(BigInt256),
+}
+
+impl Default for ShiftedValue {
+    fn default() -> Self {
+        Self::ShiftedValue(BigInt256::default())
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+#[wire_type(recurse = 2)]
+pub struct SpongeDigestBeforeEvaluations((Hex64, Hex64, Hex64, Hex64, ()));
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct ProofStatePairingBased {
+    pub sg: FiniteECPoint,
+    pub old_bulletproof_challenges: ProofStateBulletproofChallenges,
+}
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct PairingBased {
+    pub app_state: (),
+    pub sg: FiniteECPointVec,
+    pub old_bulletproof_challenges: BulletproofChallenges,
+}
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct PrevEvals((ProofEvaluations, ProofEvaluations));
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct PrevXHat(FiniteECPoint);
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+#[wire_type(recurse = 2)]
+pub struct Proof {
+    pub messages: ProofMessages,
+    pub openings: ProofOpenings,
+}
+
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug, WireType)]
+#[serde(from = "<Self as WireType>::WireType")]
+#[serde(into = "<Self as WireType>::WireType")]
+pub struct ProofOpenings {
+    pub proof: OpeningProof,
+    pub evals: (ProofEvaluations, ProofEvaluations),
+}
