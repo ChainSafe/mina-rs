734c7143133bcdd71d688e4479a9346892fd2935
Willem Olding <willemolding@gmail.com>
2021-11-17 12:05:29 +1100

    fmt

--- snark-keys/src/error.rs
+++ snark-keys/src/error.rs
@@ -5,16 +5,16 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-	#[error("Error reading snark key")]
+    #[error("Error reading snark key")]
     ReadError(#[from] std::io::Error),
 
-	#[error("Error deserializing header JSON")]
+    #[error("Error deserializing header JSON")]
     HeaderDeserializationError(#[from] serde_json::Error),
 
-	#[error("Expected first line to be 'MINA_SNARK_KEYS', got '{0}'")]
+    #[error("Expected first line to be 'MINA_SNARK_KEYS', got '{0}'")]
     FileIdError(String),
-	
-	#[error("Reached the end of the file")]
+
+    #[error("Reached the end of the file")]
     UnexpectedEndOfFileError,
 }
 
--- snark-keys/src/lib.rs
+++ snark-keys/src/lib.rs
@@ -1,8 +1,7 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0
 
-
-use std::io::{Read, BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read};
 
 mod header;
 use header::{KeyFileHeader, FILE_ID};
@@ -10,36 +9,36 @@ use header::{KeyFileHeader, FILE_ID};
 mod error;
 use error::{Error, Result};
 
-pub fn read_snark_key_file<R: Read>(r: R) -> Result<KeyFileHeader>  {
-	let mut r = BufReader::new(r);
-	read_file_id(&mut r)?;
-	let header = read_header(&mut r)?;
-	Ok(header)
+pub fn read_snark_key_file<R: Read>(r: R) -> Result<KeyFileHeader> {
+    let mut r = BufReader::new(r);
+    read_file_id(&mut r)?;
+    let header = read_header(&mut r)?;
+    Ok(header)
 }
 
 fn read_file_id<R: BufRead>(r: &mut R) -> Result<()> {
-	match r.lines().next() {
-		Some(Ok(s)) => {
-			if s == FILE_ID {
-				Ok(())
-			} else {
-				Err(Error::FileIdError(s))
-			}
-		},
-		Some(Err(e)) => Err(e.into()),
-		None => Err(Error::FileIdError("".to_string()))
-	}
+    match r.lines().next() {
+        Some(Ok(s)) => {
+            if s == FILE_ID {
+                Ok(())
+            } else {
+                Err(Error::FileIdError(s))
+            }
+        }
+        Some(Err(e)) => Err(e.into()),
+        None => Err(Error::FileIdError("".to_string())),
+    }
 }
 
 fn read_header<R: BufRead>(r: &mut R) -> Result<KeyFileHeader> {
-	match r.lines().next() {
-		Some(Ok(s)) => {
-			let header = serde_json::from_str(&s)?;
-			Ok(header)
-		},
-		Some(Err(e)) => Err(e.into()),
-		None => Err(Error::UnexpectedEndOfFileError)
-	}
+    match r.lines().next() {
+        Some(Ok(s)) => {
+            let header = serde_json::from_str(&s)?;
+            Ok(header)
+        }
+        Some(Err(e)) => Err(e.into()),
+        None => Err(Error::UnexpectedEndOfFileError),
+    }
 }
 
 // fn read_body<R: Read>(r: &mut R) -> Result<()> {
@@ -48,12 +47,14 @@ fn read_header<R: BufRead>(r: &mut R) -> Result<KeyFileHeader> {
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	const TEST_KEYFILE: &[u8] = include_bytes!("../vk-wrap-wrap-verification-key-blockchain-snark-d3623dbfa42f563e40cd5f2d032ad91f");
+    use super::*;
+    const TEST_KEYFILE: &[u8] = include_bytes!(
+        "../vk-wrap-wrap-verification-key-blockchain-snark-d3623dbfa42f563e40cd5f2d032ad91f"
+    );
 
-	#[test]
-	fn smoke_test_read_file() {
-		let header = read_snark_key_file(TEST_KEYFILE).unwrap();
-		println!("{:?}", header);
-	}
+    #[test]
+    fn smoke_test_read_file() {
+        let header = read_snark_key_file(TEST_KEYFILE).unwrap();
+        println!("{:?}", header);
+    }
 }
