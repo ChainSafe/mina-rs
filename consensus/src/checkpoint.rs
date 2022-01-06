a1d3a8e55cb8469a53160d6a78fcf0c534409acd
Hsin-Wen Chang <bow1226@gmail.com>
2022-01-06 09:24:26 +0800

    feat: Refinement is_short_range, remove warnings

--- consensus/src/checkpoint.rs
+++ consensus/src/checkpoint.rs
@@ -49,6 +49,7 @@ pub fn is_short_range(
     c0: &ProtocolStateChain,
     c1: &ProtocolStateChain,
 ) -> Result<bool, ConsensusErrTyp> {
+    // Get consensus state from top blocks of each chain
     let s0 = &c0
         .consensus_state()
         .ok_or(ConsensusErrTyp::ConsensusInitFail)?;
@@ -58,16 +59,24 @@ pub fn is_short_range(
     let s0_lock_checkpoint = &s0.staking_epoch_data.lock_checkpoint;
     let s1_lock_checkpoint = &s1.staking_epoch_data.lock_checkpoint;
     let s1_next_epoch_lock_checkpoint = &s1.next_epoch_data.lock_checkpoint;
+    let check = |s0_lock_checkpoint, s1_next_epoch_lock_checkpoint| {
+        if s0.epoch_count.0 == s1.epoch_count.0 + 1
+            && c1.epoch_slot() >= Some(SLOTS_PER_EPOCH * 2 / 3)
+        {
+            // S1 is one epoch ahead of S2 and S2 is not in the seed update range
+            s0_lock_checkpoint == s1_next_epoch_lock_checkpoint
+        } else {
+            false
+        }
+    };
 
     if s0.epoch_count == s1.epoch_count {
+        // Simple case: blocks have same previous epoch, so compare previous epochs' lock_checkpoints
         return Ok(s0_lock_checkpoint == s1_lock_checkpoint);
-    }
-
-    if s0.epoch_count.0 == s1.epoch_count.0 + 1 && c1.epoch_slot() >= Some(SLOTS_PER_EPOCH * 2 / 3)
-    {
-        Ok(s0_lock_checkpoint == s1_next_epoch_lock_checkpoint)
     } else {
-        Ok(false)
+        // Check for previous epoch case using both orientations
+        Ok(check(s0_lock_checkpoint, s1_next_epoch_lock_checkpoint)
+            || check(s1_next_epoch_lock_checkpoint, s0_lock_checkpoint))
     }
 }
 
@@ -81,8 +90,8 @@ mod tests {
     use proptest::prelude::*;
     use quickcheck::QuickCheck;
     use wasm_bindgen_test::*;
-    const default_slot_fill_rate: f64 = 0.65;
-    const default_slot_fill_rate_delta: f64 = 0.15;
+    const DEFAULT_SLOT_FILL_RATE: f64 = 0.65;
+    const DEFAULT_SLOT_FILL_RATE_DELTA: f64 = 0.15;
 
     #[test]
     fn test_init_checkpoints() {
@@ -175,14 +184,14 @@ mod tests {
         // can be used in other generators that wish to generates pairs of spot blocks
         // with specific constraints.
         let base_root_epoch_position =
-            gen_spot_root_epoch_position(default_slot_fill_rate, default_slot_fill_rate_delta);
+            gen_spot_root_epoch_position(DEFAULT_SLOT_FILL_RATE, DEFAULT_SLOT_FILL_RATE_DELTA);
 
         // Constraining the second state to have a greater blockchain length than the
         // first, we need to constrain the first blockchain length such that there is some room
         // leftover in the epoch for at least 1 more block to be generated.
-        let blockchain_length_relativity = Some(&a);
-        let max_epoch_slot = match blockchain_length_relativity {
-            Some(blockchain_length_relativity) => {
+        let _blockchain_length_relativity = Some(&a);
+        let max_epoch_slot = match _blockchain_length_relativity {
+            Some(_blockchain_length_relativity) => {
                 a.body.consensus_state.curr_global_slot.slots_per_epoch.0 - 4
             } // -1 to bring into inclusive range, -3 to provide 2 slots of fudge room
             None => a.body.consensus_state.curr_global_slot.slots_per_epoch.0 - 1, // -1 to bring into inclusive range
@@ -192,8 +201,8 @@ mod tests {
         let min_a_curr_epoch_slot_sum = min_a_curr_epoch_slot_defaut + min_a_curr_epoch_slot;
         let slot = thread_rng().gen_range(min_a_curr_epoch_slot_sum..max_epoch_slot);
         let length = gen_num_blocks_in_slots(
-            default_slot_fill_rate,
-            default_slot_fill_rate_delta,
+            DEFAULT_SLOT_FILL_RATE,
+            DEFAULT_SLOT_FILL_RATE_DELTA,
             slot as f64,
         );
 
@@ -223,8 +232,8 @@ mod tests {
         let added_slots = thread_rng().gen_range(a_curr_epoch_slot.0 + 2..max_epoch_slot);
 
         let added_blocks = gen_num_blocks_in_slots(
-            default_slot_fill_rate,
-            default_slot_fill_rate_delta,
+            DEFAULT_SLOT_FILL_RATE,
+            DEFAULT_SLOT_FILL_RATE_DELTA,
             added_slots as f64,
         );
 
@@ -245,11 +254,10 @@ mod tests {
         gen_spot(&mut a);
         gen_spot(&mut b);
 
-        let mut c0: ProtocolStateChain = ProtocolStateChain(vec![a]);
-        let mut c1: ProtocolStateChain = ProtocolStateChain(vec![b]);
+        let c0: ProtocolStateChain = ProtocolStateChain(vec![a]);
+        let c1: ProtocolStateChain = ProtocolStateChain(vec![b]);
 
         assert_eq!(is_short_range(&c0, &c1).unwrap(), true);
-        assert_eq!(is_short_range(&c1, &c0).unwrap(), true);
     }
 
     #[test]
@@ -266,11 +274,10 @@ mod tests {
         gen_spot(&mut b);
         gen_spot_pair_common_checkpoints(&mut a, &mut b, 0);
 
-        let mut c0: ProtocolStateChain = ProtocolStateChain(vec![a]);
-        let mut c1: ProtocolStateChain = ProtocolStateChain(vec![b]);
+        let c0: ProtocolStateChain = ProtocolStateChain(vec![a]);
+        let c1: ProtocolStateChain = ProtocolStateChain(vec![b]);
 
         assert_eq!(is_short_range(&c0, &c1).unwrap(), true);
-        assert_eq!(is_short_range(&c1, &c0).unwrap(), true);
     }
 
     #[test]
@@ -287,7 +294,7 @@ mod tests {
 
         // Compute the root epoch position of `b`. This needs to be one epoch ahead of a
         let added_blocks =
-            gen_num_blocks_in_slots(default_slot_fill_rate, default_slot_fill_rate_delta, 1.0);
+            gen_num_blocks_in_slots(DEFAULT_SLOT_FILL_RATE, DEFAULT_SLOT_FILL_RATE_DELTA, 1.0);
 
         b.body.consensus_state.blockchain_length.0 =
             a.body.consensus_state.blockchain_length.0 + added_blocks as u32;
@@ -297,11 +304,10 @@ mod tests {
         let min_a_curr_epoch_slot = 2 * (protocol_constants.slots_per_epoch.0 / 3) + 1;
         gen_spot_pair_common_checkpoints(&mut a, &mut b, min_a_curr_epoch_slot);
 
-        let mut c0: ProtocolStateChain = ProtocolStateChain(vec![a]);
-        let mut c1: ProtocolStateChain = ProtocolStateChain(vec![b]);
+        let c0: ProtocolStateChain = ProtocolStateChain(vec![a]);
+        let c1: ProtocolStateChain = ProtocolStateChain(vec![b]);
 
         assert_eq!(is_short_range(&c0, &c1).unwrap(), true);
-        assert_eq!(is_short_range(&c1, &c0).unwrap(), true);
     }
 
     #[test]
@@ -317,10 +323,9 @@ mod tests {
         gen_spot(&mut b);
         a.body.consensus_state.epoch_count = Length(14);
         b.body.consensus_state.epoch_count = Length(15);
-        let mut c0: ProtocolStateChain = ProtocolStateChain(vec![a]);
-        let mut c1: ProtocolStateChain = ProtocolStateChain(vec![b]);
+        let c0: ProtocolStateChain = ProtocolStateChain(vec![a]);
+        let c1: ProtocolStateChain = ProtocolStateChain(vec![b]);
 
         assert_eq!(is_short_range(&c0, &c1).unwrap(), false);
-        assert_eq!(is_short_range(&c1, &c0).unwrap(), false);
     }
 }
