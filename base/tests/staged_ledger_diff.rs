07efe91a1bb94d7e1a1d79e9d52af9f6ef67cddd
Hsin-Wen Chang <bow1226@gmail.com>
2022-01-12 10:12:48 +0800

    feat: Init

---
+++ base/tests/staged_ledger_diff.rs
@@ -0,0 +1,36 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0
+
+#[cfg(all(test, feature = "browser"))]
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+#[cfg(test)]
+mod tests {
+    use mina_crypto::base58::Base58Encodable;
+    use mina_rs_base::staged_ledger_diff::{
+        CoinBase, CoinBaseBalanceData, CoinBaseFeeTransfer, FeeTransferBalanceData, SnappCommand,
+        TransactionStatus,
+    };
+    use mina_rs_base::types::*;
+    use time::macros::*;
+    use wasm_bindgen_test::*;
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_CoinBaseFeeTransfer() {}
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_CoinBaseBalanceData() {}
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_FeeTransferBalanceData() {}
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_CoinBase() {}
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_TransactionStatus() {}
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_SnappCommand() {}
+}
