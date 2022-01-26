17f8a902ddc5c46b10722d20e3ceae953b85162f
creativcoder <5155745+creativcoder@users.noreply.github.com>
2022-01-26 13:44:14 +0530

    minor refactors around consensus and use actual is_short_range API (#128)
    * minor refactors around error handling and use actual is_short_range API
    
    * relocate init_checkpoints as they are only used in tests
    
    * nit: clippy fixes

--- consensus/src/checkpoint.rs
+++ consensus/src/checkpoint.rs
@@ -3,57 +3,18 @@
 
 use crate::common::{Chain, ProtocolStateChain};
 use crate::constants::SLOTS_PER_EPOCH;
-use mina_crypto::base58::Base58Encodable;
-use mina_crypto::hash::{EpochSeed, StateHash};
-use mina_rs_base::protocol_state::ProtocolState;
-
-#[derive(Debug)]
-pub enum ConsensusErrTyp {
-    ConsensusInitFail,
-}
-pub fn init_checkpoints(genesis: &mut ProtocolState) -> Result<(), ConsensusErrTyp> {
-    genesis.body.consensus_state.staking_epoch_data.seed = EpochSeed::default();
-    genesis
-        .body
-        .consensus_state
-        .staking_epoch_data
-        .start_checkpoint = StateHash::default();
-    genesis
-        .body
-        .consensus_state
-        .staking_epoch_data
-        .lock_checkpoint = StateHash::default();
-    genesis
-        .body
-        .consensus_state
-        .staking_epoch_data
-        .epoch_length
-        .0 = 1;
-    genesis.body.consensus_state.next_epoch_data.seed =
-        EpochSeed::from_base58("2vaRh7FQ5wSzmpFReF9gcRKjv48CcJvHs25aqb3SSZiPgHQBy5Dt")
-            .map_err(|_| ConsensusErrTyp::ConsensusInitFail)?;
-    genesis
-        .body
-        .consensus_state
-        .next_epoch_data
-        .start_checkpoint = StateHash::default();
-    genesis.body.consensus_state.next_epoch_data.lock_checkpoint =
-        StateHash::from_base58("3NLoKn22eMnyQ7rxh5pxB6vBA3XhSAhhrf7akdqS6HbAKD14Dh1d")
-            .map_err(|_| ConsensusErrTyp::ConsensusInitFail)?;
-    genesis.body.consensus_state.next_epoch_data.epoch_length.0 = 2;
-    Ok(())
-}
+use crate::error::ConsensusError;
 
 pub fn is_short_range(
     c0: &ProtocolStateChain,
     c1: &ProtocolStateChain,
-) -> Result<bool, ConsensusErrTyp> {
+) -> Result<bool, ConsensusError> {
     let s0 = &c0
         .consensus_state()
-        .ok_or(ConsensusErrTyp::ConsensusInitFail)?;
+        .ok_or(ConsensusError::ConsensusStateNotFound)?;
     let s1 = &c1
         .consensus_state()
-        .ok_or(ConsensusErrTyp::ConsensusInitFail)?;
+        .ok_or(ConsensusError::ConsensusStateNotFound)?;
     let s0_lock_checkpoint = &s0.staking_epoch_data.lock_checkpoint;
     let s1_lock_checkpoint = &s1.staking_epoch_data.lock_checkpoint;
     let s1_next_epoch_lock_checkpoint = &s1.next_epoch_data.lock_checkpoint;
