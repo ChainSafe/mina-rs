3b97a981b4cee0ea4345f07de23e235002a6121d
Willem Olding <willemolding@gmail.com>
2022-02-03 13:47:09 +1100

    rename to serialization-types

--- protocol/test-fixtures/src/lib.rs
+++ protocol/test-fixtures/src/lib.rs
@@ -6,7 +6,7 @@
 use bin_prot::{BinProtRule, Deserializer};
 use lazy_static::lazy_static;
 use mina_crypto::prelude::*;
-use mina_network_types::v1::ExternalTransitionV1;
+use mina_serialization_types::v1::ExternalTransitionV1;
 use mina_rs_base::external_transition::ExternalTransition;
 use serde::Deserialize;
 use std::{borrow::Borrow, collections::HashMap};
