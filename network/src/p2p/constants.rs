73f4080a0721d7888d320ee3189892c2b064b9ee
Hsin-Wen Chang <bow1226@gmail.com>
2022-02-08 16:46:00 +0800

    fmt

--- network/src/p2p/constants.rs
+++ network/src/p2p/constants.rs
@@ -1,5 +1,4 @@
 // Copyright 2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0
 
-pub const DNS_DEST: &str =
-    "/dns4/seed-1.devnet.o1test.net/tcp/10000/p2p/12D3KooWAdgYL6hv18M3iDBdaK1dRygPivSfAfBNDzie6YqydVbs";
+pub const DNS_DEST: &str = "/ip4/0.0.0.0/tcp/0";
