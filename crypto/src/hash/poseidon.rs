e7abc7dea110f07dab33811ca3bcc88d7b335d54
hanabi1224 <harlowmoo@gmail.com>
2022-02-15 23:53:08 +0800

    Update comments on which type Fp refers to

--- crypto/src/hash/poseidon.rs
+++ crypto/src/hash/poseidon.rs
@@ -5,8 +5,10 @@ use ark_ff::{biginteger::BigInteger256, FromBytes};
 use mina_curves::pasta::*;
 use oracle::poseidon::*;
 
-/// Converts prefix string into field type of pasta elliptic curve
-pub fn prefix_to_field(s: &[u8]) -> Result<mina_curves::pasta::Fp, std::io::Error> {
+/// Converts prefix string into scalar type of vesta elliptic curve
+/// Note that Fp is the base field type of pallas elliptic curve and
+/// the scalar field type of vesta elliptic curve
+pub fn prefix_to_field(s: &[u8]) -> Result<Fp, std::io::Error> {
     const LEN: usize = 32;
     let mut bytes = [0_u8; LEN];
     for (i, &b) in s.iter().enumerate().take(LEN) {
