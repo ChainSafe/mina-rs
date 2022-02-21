e510af19293430f9edbd2ac2c2885eba4cf622bc
hanabi1224 <harlowmoo@gmail.com>
2022-02-22 01:31:33 +0800

    type alias for vesta scalar field

--- crypto/src/hash/poseidon.rs
+++ crypto/src/hash/poseidon.rs
@@ -7,32 +7,30 @@ use mina_curves::pasta::vesta;
 use o1_utils::field_helpers::{FieldHelpers, FieldHelpersError};
 use oracle::poseidon::*;
 
+/// Alias type for [<mina_curves::pasta::vesta::Affine as ark_ec::AffineCurve>::ScalarField]
+pub type VestaScalarField = <vesta::Affine as AffineCurve>::ScalarField;
+
 /// Converts prefix string into scalar type of vesta elliptic curve
 /// Note that Fp is the base field type of pallas elliptic curve and
 /// the scalar field type of vesta elliptic curve
-pub fn prefix_to_field(
-    s: &[u8],
-) -> Result<<vesta::Affine as AffineCurve>::ScalarField, FieldHelpersError> {
+pub fn prefix_to_field(s: &[u8]) -> Result<VestaScalarField, FieldHelpersError> {
     // Need to pad bytes into MODULUS_BITS bits
     // All predefined prefixes are 160 bits
-    const BITS: u32 = <<<vesta::Affine as AffineCurve>::ScalarField as PrimeField>::Params as FpParameters>::MODULUS_BITS;
+    const BITS: u32 = <<VestaScalarField as PrimeField>::Params as FpParameters>::MODULUS_BITS;
     const LEN: usize = bits_to_byte_len(BITS as usize);
     let mut bytes = [0_u8; LEN];
     for (i, &b) in s.iter().enumerate().take(LEN) {
         bytes[i] = b;
     }
-    <vesta::Affine as AffineCurve>::ScalarField::from_bytes(&bytes)
+    VestaScalarField::from_bytes(&bytes)
 }
 
 /// Gets poseidon hash state from prefix string
-pub fn salt(
-    s: &[u8],
-) -> Result<Vec<<vesta::Affine as AffineCurve>::ScalarField>, FieldHelpersError> {
+pub fn salt(s: &[u8]) -> Result<Vec<VestaScalarField>, FieldHelpersError> {
     let f = prefix_to_field(s)?;
-    let mut hash = ArithmeticSponge::<
-        <vesta::Affine as AffineCurve>::ScalarField,
-        PlonkSpongeConstantsBasic,
-    >::new(oracle::pasta::fp::params());
+    let mut hash = ArithmeticSponge::<VestaScalarField, PlonkSpongeConstantsBasic>::new(
+        oracle::pasta::fp::params(),
+    );
     hash.absorb(&[f]);
     hash.squeeze();
     Ok(hash.state)
@@ -40,14 +38,10 @@ pub fn salt(
 
 /// Calculates poseidon hash of given fields with init state
 /// This hash algorithm is used for implementing mina merkle tree hasher and merger
-pub fn hash(
-    init_state: Vec<<vesta::Affine as AffineCurve>::ScalarField>,
-    fields: &[<vesta::Affine as AffineCurve>::ScalarField],
-) -> <vesta::Affine as AffineCurve>::ScalarField {
-    let mut hash = ArithmeticSponge::<
-        <vesta::Affine as AffineCurve>::ScalarField,
-        PlonkSpongeConstantsBasic,
-    >::new(oracle::pasta::fp::params());
+pub fn hash(init_state: Vec<VestaScalarField>, fields: &[VestaScalarField]) -> VestaScalarField {
+    let mut hash = ArithmeticSponge::<VestaScalarField, PlonkSpongeConstantsBasic>::new(
+        oracle::pasta::fp::params(),
+    );
     hash.state = init_state;
     hash.absorb(fields);
     hash.squeeze()
