0e3d202a73cf7a7e478f6b3e1b7eb66598cc95fb
Willem Olding <willemolding@gmail.com>
2022-02-28 10:12:32 +1100

    replace placeholder comments

--- protocol/bin-prot/src/encodable.rs
+++ protocol/bin-prot/src/encodable.rs
@@ -4,10 +4,10 @@ use serde::{Deserialize, Serialize};
 
 /// Trait to allow a type to give a size it will serialize to/from
 pub trait BinProtEncodable {
-    /// yep
+    /// Size of buffer to allocate to serialialized form. This required prior knowledge.
     const PREALLOCATE_BUFFER_BYTES: usize;
 
-    /// yep
+    /// Tries to encode self into a preallocated buffer of bytes in bin-prot form
     fn try_encode_binprot(&self) -> Result<Vec<u8>, Error>
     where
         Self: Serialize,
@@ -17,7 +17,7 @@ pub trait BinProtEncodable {
         Ok(output)
     }
 
-    /// yep
+    /// Try to decode some bytes into the implementing type interpreting them as bin-prot encoding
     fn try_decode_binprot<'de>(bytes: impl AsRef<[u8]>) -> Result<Self, Error>
     where
         Self: Deserialize<'de> + Sized,
