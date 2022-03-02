d3e3518002b24de66aadd642c6644b34ca83c40f
hanabi1224 <harlowmoo@gmail.com>
2022-03-02 15:03:29 +0800

    Speed up ci job (#168)

--- scripts/profiler.sh
+++ scripts/profiler.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-pushd "$(dirname $0)/../apps/target/release"
+pushd "$(dirname $0)/../apps"
+cargo build --release -p mina-rs-profilers
+pushd "target/release"
 mkdir profiler-reports || echo 'dir exists'
 mkdir profiler-reports/block-serde-profiler || echo 'dir exists'
 pushd profiler-reports/block-serde-profiler
@@ -9,5 +11,6 @@ pushd profiler-reports/block-serde-profiler
 valgrind --tool=callgrind --callgrind-out-file=callgrind.out --  ./../../block-serde-profiler -m cpu
 gprof2dot -f callgrind callgrind.out | dot -Tsvg -o callgrind.svg
 
+popd
 popd
 popd
