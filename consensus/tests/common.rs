1d18efb59de99260a9e274042d19ab6ae93e4971
sudo-shashank <shashank@chainsafe.io>
2022-03-03 09:16:54 +0530

    Added select secure chain test cases

--- consensus/tests/common.rs
+++ consensus/tests/common.rs
@@ -256,4 +256,231 @@ mod tests {
             VrfOutputTruncated::from("kKr83LYd7DyFupRAPh5Dh9eWM1teSEs5VjU4XId2DgA=")
         );
     }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_select_secure_chain_short_range_fork() {
+        let mut chain_a = ProtocolStateChain::default();
+        let mut consensus_state = ConsensusState::default();
+        consensus_state.epoch_count = Length(5);
+        consensus_state.blockchain_length = Length(11);
+        let mut prot_state = ProtocolState::default();
+        prot_state.body.consensus_state = consensus_state;
+        chain_a.push(prot_state).unwrap();
+
+        let mut chain_b = ProtocolStateChain::default();
+        let mut consensus_state = ConsensusState::default();
+        consensus_state.epoch_count = Length(5);
+        consensus_state.blockchain_length = Length(10);
+        let mut prot_state = ProtocolState::default();
+        prot_state.body.consensus_state = consensus_state;
+        chain_b.push(prot_state).unwrap();
+
+        let mut chains = vec![];
+        chains.push(chain_b);
+        let select_result = chain_a.select_secure_chain(&chains).unwrap();
+        let result_state = select_result.0.get(0).unwrap();
+        assert_eq!(
+            result_state.body.consensus_state.blockchain_length,
+            Length(11)
+        );
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    //Long range fork with greater relative min window density
+    fn test_select_secure_chain_long_range_fork_greater_relative_min_window_density() {
+        let mut chain_a = ProtocolStateChain::default();
+        let mut consensus_state = ConsensusState::default();
+        consensus_state.epoch_count = Length(23);
+        consensus_state.min_window_density = Length(77);
+        consensus_state.sub_window_densities = vec![
+            Length(1),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+        ];
+        consensus_state.curr_global_slot = GlobalSlot {
+            slot_number: GlobalSlotNumber(0),
+            slots_per_epoch: Length(7140),
+        };
+        consensus_state.blockchain_length = Length(1);
+        let mut prot_state = ProtocolState::default();
+        prot_state.body.consensus_state = consensus_state;
+        chain_a.push(prot_state).unwrap();
+
+        let mut chain_b = ProtocolStateChain::default();
+        let mut consensus_state = ConsensusState::default();
+        consensus_state.epoch_count = Length(15);
+        consensus_state.min_window_density = Length(14);
+        consensus_state.sub_window_densities = vec![
+            Length(7),
+            Length(2),
+            Length(2),
+            Length(5),
+            Length(6),
+            Length(7),
+            Length(5),
+            Length(7),
+            Length(5),
+            Length(5),
+            Length(5),
+        ];
+        consensus_state.curr_global_slot = GlobalSlot {
+            slot_number: GlobalSlotNumber(167176),
+            slots_per_epoch: Length(7140),
+        };
+        consensus_state.blockchain_length = Length(77748);
+        let mut prot_state = ProtocolState::default();
+        prot_state.body.consensus_state = consensus_state;
+        chain_b.push(prot_state).unwrap();
+
+        let mut chains = vec![];
+        chains.push(chain_b);
+        let select_result = chain_a.select_secure_chain(&chains).unwrap();
+        let result_state = select_result.0.get(0).unwrap();
+        assert_eq!(
+            result_state.body.consensus_state.blockchain_length,
+            Length(77748)
+        );
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    ///Long range fork with same relative min window density
+    fn test_select_secure_chain_long_range_fork_same_relative_min_window_density() {
+        let mut chain_a = ProtocolStateChain::default();
+        let mut consensus_state = ConsensusState::default();
+        consensus_state.epoch_count = Length(23);
+        consensus_state.min_window_density = Length(14);
+        consensus_state.sub_window_densities = vec![
+            Length(7),
+            Length(2),
+            Length(2),
+            Length(5),
+            Length(6),
+            Length(7),
+            Length(5),
+            Length(7),
+            Length(5),
+            Length(5),
+            Length(5),
+        ];
+        consensus_state.curr_global_slot = GlobalSlot {
+            slot_number: GlobalSlotNumber(167176),
+            slots_per_epoch: Length(7140),
+        };
+        consensus_state.blockchain_length = Length(1);
+        let mut prot_state = ProtocolState::default();
+        prot_state.body.consensus_state = consensus_state;
+        chain_a.push(prot_state).unwrap();
+
+        let mut chain_b = ProtocolStateChain::default();
+        let mut consensus_state = ConsensusState::default();
+        consensus_state.epoch_count = Length(15);
+        consensus_state.min_window_density = Length(14);
+        consensus_state.sub_window_densities = vec![
+            Length(7),
+            Length(2),
+            Length(2),
+            Length(5),
+            Length(6),
+            Length(7),
+            Length(5),
+            Length(7),
+            Length(5),
+            Length(5),
+            Length(5),
+        ];
+        consensus_state.curr_global_slot = GlobalSlot {
+            slot_number: GlobalSlotNumber(167176),
+            slots_per_epoch: Length(7140),
+        };
+        consensus_state.blockchain_length = Length(77748);
+        let mut prot_state = ProtocolState::default();
+        prot_state.body.consensus_state = consensus_state;
+        chain_b.push(prot_state).unwrap();
+
+        let mut chains = vec![];
+        chains.push(chain_b);
+        let select_result = chain_a.select_secure_chain(&chains).unwrap();
+        let result_state = select_result.0.get(0).unwrap();
+        assert_eq!(
+            result_state.body.consensus_state.blockchain_length,
+            Length(77748)
+        );
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    //Long range fork with lesser relative min window density
+    fn test_select_secure_chain_long_range_fork_lesser_relative_min_window_density() {
+        let mut chain_a = ProtocolStateChain::default();
+        let mut consensus_state = ConsensusState::default();
+        consensus_state.epoch_count = Length(15);
+        consensus_state.min_window_density = Length(14);
+        consensus_state.sub_window_densities = vec![
+            Length(7),
+            Length(2),
+            Length(2),
+            Length(5),
+            Length(6),
+            Length(7),
+            Length(5),
+            Length(7),
+            Length(5),
+            Length(5),
+            Length(5),
+        ];
+        consensus_state.curr_global_slot = GlobalSlot {
+            slot_number: GlobalSlotNumber(167176),
+            slots_per_epoch: Length(7140),
+        };
+        consensus_state.blockchain_length = Length(77748);
+        let mut prot_state = ProtocolState::default();
+        prot_state.body.consensus_state = consensus_state;
+        chain_a.push(prot_state).unwrap();
+
+        let mut chain_b = ProtocolStateChain::default();
+        let mut consensus_state = ConsensusState::default();
+        consensus_state.epoch_count = Length(23);
+        consensus_state.min_window_density = Length(77);
+        consensus_state.sub_window_densities = vec![
+            Length(1),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+            Length(7),
+        ];
+        consensus_state.curr_global_slot = GlobalSlot {
+            slot_number: GlobalSlotNumber(10),
+            slots_per_epoch: Length(7140),
+        };
+        consensus_state.blockchain_length = Length(1);
+        let mut prot_state = ProtocolState::default();
+        prot_state.body.consensus_state = consensus_state;
+        chain_b.push(prot_state).unwrap();
+
+        let mut chains = vec![];
+        chains.push(chain_b);
+        let select_result = chain_a.select_secure_chain(&chains).unwrap();
+        let result_state = select_result.0.get(0).unwrap();
+        assert_eq!(
+            result_state.body.consensus_state.blockchain_length,
+            Length(77748)
+        );
+    }
 }
