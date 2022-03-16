10997a5765b1599cf896a23375c162620764d235
hanabi1224 <harlowmoo@gmail.com>
2022-03-16 19:38:23 +0800

    lodash

--- apps/wasm/mvp/web/index.ts
+++ apps/wasm/mvp/web/index.ts
@@ -1,12 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import 'regenerator-runtime/runtime'
-import { initWasm } from "./../utils"
 
-async function buildApp() {
-  await initWasm()
-  const app = createApp(App);
-  app.mount("#app");
-}
-
-buildApp()
+const app = createApp(App);
+app.mount("#app");
