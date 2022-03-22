b4ccdc1262bc5add9c664322a56eb15294f116a2
hanabi1224 <harlowmoo@gmail.com>
2022-03-22 13:19:02 +0800

    cleanup

--- apps/wasm/mvp/patch.js
+++ apps/wasm/mvp/patch.js
@@ -5,7 +5,6 @@ function patchWasmJs() {
     let content = fs.readFileSync(filePath, { encoding: 'utf-8' })
     content = content
         .replace(/const(.+?)\s*=\s*require\s*\(String\.raw`(.+?)`\).+/g, 'import $1 from "$2"')
-        // .replace(/:\s*Text.+?coder\d/g, '')
         .replace('const { Text', '// const { Text')
         .replace('const path = ', '/* const path = ')
     content += `
