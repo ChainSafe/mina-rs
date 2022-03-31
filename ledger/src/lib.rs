44c0793a6fda8022c201a57ae46732ac55e9e526
Willem Olding <willemolding@gmail.com>
2022-03-31 11:16:50 +1100

    Willem/genesis ledger implementation (#183)
    * adds genesis ledger with minimal interface
    
    * add doc comments
    
    * add descriptor for iterator
    
    * adds test against rocksdb instance
    
    * add proper error handling
    
    * remove empty error file
    
    * do not include the rocksdb backend when compiling for a wasm target
    
    * add missing part of test data that gitignore skipped
    
    * save bytes
    
    * fmt

---
+++ ledger/src/lib.rs
@@ -0,0 +1,15 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0
+
+//! Functionality relating to the ledger or genesis ledgers
+
+#![deny(warnings)]
+#![deny(missing_docs)]
+
+mod genesis_ledger;
+pub use genesis_ledger::GenesisLedger;
+
+#[cfg(not(target_arch = "wasm32"))]
+mod rocksdb_genesis_ledger;
+#[cfg(not(target_arch = "wasm32"))]
+pub use rocksdb_genesis_ledger::RocksDbGenesisLedger;
