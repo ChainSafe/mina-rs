53aef861ae092ffecd3cbfe9f31c3dc07c06a790
Willem Olding <willemolding@gmail.com>
2022-04-05 15:46:53 +1000

    adds a demo extracting the TransactionSnarkWork using the loose deserializer

---
+++ protocol/serialization-types/tests/test_deserialize_snark_work.rs
@@ -0,0 +1,22 @@
+
+use test_fixtures::TEST_BLOCKS;
+
+
+#[test]
+fn test_deserialize_snark_work() {
+	// Grab a block we know to contain snark work from the fixtures
+	let block_fixture = TEST_BLOCKS.get("3NK9fHpzfPWhuxFhQ9Dau1X1JWtstB6kGC4xrurSPU1kctMCsU9U.hex").unwrap();
+
+	// using the loosely deserialized form, extract the first snark work component
+	// path: t/staged_ledger_diff/t/diff/t/0/t/t/completed_works/0
+	let snark_work_example: &bin_prot::Value = &block_fixture.value["t"]["staged_ledger_diff"]["t"]["diff"]["t"][0]["t"]["t"]["completed_works"][0];
+	println!("{:?}", snark_work_example);
+
+	// we can reserialize the loosely deserialized version to get some bytes for the example snark work
+	let mut snark_work_example_bytes = Vec::new();
+	bin_prot::to_writer(&mut snark_work_example_bytes, snark_work_example).unwrap();
+	// you could write these to a file and try to deserialize them directly into the TransactionSnarkWork type 
+	// to make test turnaround time much quicker
+
+	println!("{:?}", snark_work_example_bytes);
+}
