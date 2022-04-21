98e2d5133794944c084d16b907db368605f2947b
hanabi1224 <harlowmoo@gmail.com>
2022-04-21 11:04:45 +0800

    doc

--- protocol/serialization-types/src/type_annotation.rs
+++ protocol/serialization-types/src/type_annotation.rs
@@ -1,12 +1,12 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0
 
-/// This trait annotates a given type its different serializable types,
+/// This trait annotates a given type its different serialization types,
 /// and provide utility functions to easily convert between them
 pub trait TypeAnnotation: Sized {
-    /// The corresponding serializable type for bin-prot format
+    /// The corresponding serialization type for bin-prot format
     type BinProtType: From<Self> + Into<Self>;
 
-    /// The corresponding serializable type for json format
+    /// The corresponding serialization type for json format
     type JsonType: From<Self> + Into<Self>;
 }
