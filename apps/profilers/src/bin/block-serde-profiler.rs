9ac71d3fcc621ce3ffc5ac778ad55ff575958488
hanabi1224 <harlowmoo@gmail.com>
2022-04-25 09:54:30 +0800

    fix

--- apps/profilers/src/bin/block-serde-profiler.rs
+++ apps/profilers/src/bin/block-serde-profiler.rs
@@ -31,7 +31,7 @@ fn main() -> anyhow::Result<()> {
 
 fn cpu_profile_serialization() -> anyhow::Result<ExternalTransition> {
     let mut de = bin_prot::Deserializer::from_reader(BLOCK_BYTES);
-    let et: <ExternalTransition as SerializationTypeAnnotation>::BinProtType =
+    let et: <ExternalTransition as BinProtSerializationType>::T =
         serde::Deserialize::deserialize(&mut de)?;
     Ok(et.into())
 }
