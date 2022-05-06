7830bc6743c8dbfd87f87f64b947ee5a87f64a54
hanabi1224 <harlowmoo@gmail.com>
2022-05-06 22:21:54 +0800

    Update MVP dependency packages (#225)
    * Update deps of MVP
    
    * exclude generated rs files

--- apps/wasm/mvp/src/pb.rs
+++ apps/wasm/mvp/src/pb.rs
@@ -1,6 +1,4 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0
 
-pub mod messages;
-pub mod requests;
-pub mod responses;
+include!(concat!(env!("OUT_DIR"), "/protobuf/mod.rs"));
