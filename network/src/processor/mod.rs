46164c6ef6fdbd4629b2e2542458636ef0d664ac
hanabi1224 <harlowmoo@gmail.com>
2022-05-19 19:39:20 +0800

    docs

--- network/src/processor/mod.rs
+++ network/src/processor/mod.rs
@@ -5,62 +5,78 @@
 //! Implementation agnostic abstraction of networking
 //! operations and the transition frontier for mina node(s).
 //!
-//! The goal of this module is to support both graphql backend and libp2p backend
-//! to power both wasm node(s) that run in browser and cli node(s)
-//! that run on x86-64 and arm-64 CPU(s). It also allows easily mocking
-//! the networking layer to unittest [TransitionFrontier]
+//! The goal of this module is to provide extensible traits to
+//! support graphql and libp2p backend, etc. to power
+//! both wasm node(s) that run in browser and cli node(s)
+//! that run on x86-64 and arm-64 CPU(s). It also makes it easy to mock
+//! the networking layer to unittest the [TransitionFrontier]
 //!
 
 mod processor_impl;
 
+use async_trait::async_trait;
 use tokio::sync::mpsc;
 
 /// Request struct for querying a block
 pub struct QueryBlockRequest {
-    /// Height of parent block
+    /// Height of the block
     pub height: usize,
-    /// State hash of parent block
+    /// State hash of the block
     pub state_hash: String,
 }
 
-/// TODO: Doc
-/// Should this below to its own crate?
+/// This struct handles the blocks that are received from the
+/// network and has the capability of interacting asynchronously
+/// with the networking layer
+///
+/// TODO: Should this below to its own crate?
+#[async_trait]
 pub trait TransitionFrontier {
     /// Type that represents a block
     type Block;
 
-    /// TODO: Doc
-    fn add_block(&mut self, block: Self::Block);
+    /// Adds a block that is received from networking layer.
+    /// The block could be either pushed by other peers or
+    /// the response of [QueryBlockRequest].
+    async fn add_block(&mut self, block: Self::Block);
 
-    /// TODO: Doc
+    /// Sets the block requester for querying a block, e.g. parent of certain block
+    /// The responses will be recieved in a asynchronous way by the `add_block` API
     fn set_block_requester(&mut self, sender: mpsc::Sender<QueryBlockRequest>);
 }
 
 /// abstraction of networking operations for
 /// non-consensus mina node(s).
+#[async_trait]
 pub trait NonConsensusNetworkingOps {
     /// Type that represents a block
     type Block;
 
-    /// TODO: Doc
+    /// Sets the block responder that sends the blocks to the [TransitionFrontier]
+    ///
+    /// Note that it only assumes that new blocks are being pushed
+    /// from the network, to support polling with the graphql API(s),
+    /// there should be a separate long running function in the [NonConsensusNetworkingOps] impl
+    /// that is invoked or scheduled separately
     fn set_block_responder(&mut self, sender: mpsc::Sender<Self::Block>);
 
-    /// TODO: Doc
-    fn query_block(&self, request: &QueryBlockRequest);
+    /// Queries a block with its height and state hash
+    async fn query_block(&mut self, request: &QueryBlockRequest);
 }
 
-/// TODO: Doc
+/// This struct processes all the interactions and data exchanges
+/// between the [NonConsensusNetworkingOps] and the [TransitionFrontier]
 pub struct NetworkMessageProcessor<Block, TF, NCOps>
 where
     TF: TransitionFrontier<Block = Block>,
     NCOps: NonConsensusNetworkingOps<Block = Block>,
 {
-    /// TODO: Doc
+    /// The [TransitionFrontier] instance
     transition_frontier: TF,
-    /// TODO: Doc
+    /// The [NonConsensusNetworkingOps] instance
     nonconsensus_ops: NCOps,
-    /// TODO: Doc
+    /// Block receiver
     block_receiver: mpsc::Receiver<Block>,
-    /// TODO: Doc
-    query_parent_block_request_receiver: mpsc::Receiver<QueryBlockRequest>,
+    /// [QueryBlockRequest] receiver
+    query_block_request_receiver: mpsc::Receiver<QueryBlockRequest>,
 }
--- network/src/processor/processor_impl.rs
+++ network/src/processor/processor_impl.rs
@@ -10,53 +10,57 @@ where
     NCOps: NonConsensusNetworkingOps<Block = Block>,
     Block: Send + Sync,
 {
-    /// TODO: Doc
+    /// Creates a new [NetworkMessageProcessor] with the given [TransitionFrontier] and
+    /// [NonConsensusNetworkingOps], initializing all the message channels for the communication
     pub fn new(mut transition_frontier: TF, mut nonconsensus_ops: NCOps) -> Self {
         // TODO: make buffer size configurable, use rendezvous channel for now
         let (sender, block_receiver) = mpsc::channel::<Block>(1);
         nonconsensus_ops.set_block_responder(sender);
 
         // TODO: make buffer size configurable, use rendezvous channel for now
-        let (sender, query_parent_block_request_receiver) = mpsc::channel::<QueryBlockRequest>(1);
+        let (sender, query_block_request_receiver) = mpsc::channel::<QueryBlockRequest>(1);
         transition_frontier.set_block_requester(sender);
 
         Self {
             transition_frontier,
             nonconsensus_ops,
             block_receiver,
-            query_parent_block_request_receiver,
+            query_block_request_receiver,
         }
     }
 
-    /// TODO: Doc
+    /// Schedules event loops of all types of communications between [TransitionFrontier] and
+    /// [NonConsensusNetworkingOps].
     pub async fn run(&mut self) {
         let block_receiver = &mut self.block_receiver;
         let transition_frontier = &mut self.transition_frontier;
-        let query_parent_block_request_receiver = &mut self.query_parent_block_request_receiver;
+        let query_block_request_receiver = &mut self.query_block_request_receiver;
         let nonconsensus_ops = &mut self.nonconsensus_ops;
         tokio::select! {
             _ = Self::run_recv_block_loop(transition_frontier, block_receiver) => { }
-            _ = Self::run_query_block_loop(nonconsensus_ops, query_parent_block_request_receiver) => { }
+            _ = Self::run_query_block_loop(nonconsensus_ops, query_block_request_receiver) => { }
         };
     }
 
-    /// TODO: Doc
+    /// Schedules the event loop of sending blocks that are received from the network
+    /// to the [TransitionFrontier]
     async fn run_recv_block_loop(
         transition_frontier: &mut TF,
         block_receiver: &mut mpsc::Receiver<Block>,
     ) {
         while let Some(block) = block_receiver.recv().await {
-            transition_frontier.add_block(block);
+            transition_frontier.add_block(block).await;
         }
     }
 
-    /// TODO: Doc
+    /// Schedules the event loop of sending query-block requests
+    /// to the [NonConsensusNetworkingOps]
     async fn run_query_block_loop(
         nonconsensus_ops: &mut NCOps,
-        query_parent_block_request_receiver: &mut mpsc::Receiver<QueryBlockRequest>,
+        query_block_request_receiver: &mut mpsc::Receiver<QueryBlockRequest>,
     ) {
-        while let Some(request) = query_parent_block_request_receiver.recv().await {
-            nonconsensus_ops.query_block(&request);
+        while let Some(request) = query_block_request_receiver.recv().await {
+            nonconsensus_ops.query_block(&request).await;
         }
     }
 }
