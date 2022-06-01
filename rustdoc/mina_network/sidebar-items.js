ac46df09b55f2c48096eed9aa4a25bd92bfb9964
hanabi1224 <hanabi1224@users.noreply.github.com>
2022-06-01 11:41:59 +0000

    deploy: 46dc1db70d0204e1dbd8ae74c2b236a49e2f29a8

--- rustdoc/mina_network/sidebar-items.js
+++ rustdoc/mina_network/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["p2p","libp2p setup utilities for mina"]]});
+initSidebarItems({"mod":[["p2p","libp2p setup utilities for mina"],["processor","Implementation agnostic abstraction of networking operations and the transition frontier for mina node(s)."]]});
