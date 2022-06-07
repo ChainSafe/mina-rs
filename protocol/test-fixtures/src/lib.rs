b2cc4a1498dfa2d75eba5013c8446169c08054f4
creativcoder <creativcoders@gmail.com>
2022-06-07 09:24:39 +0530

    Update tests

--- protocol/test-fixtures/src/lib.rs
+++ protocol/test-fixtures/src/lib.rs
@@ -51,11 +51,6 @@ lazy_static! {
         "data/mainnet-147571-3NKwrze6FvGQCCF6L7Q2JLvwgnsm56hwSny9kUyjbSUr8oqu1MGp.json"
         "data/mainnet-149909-3NLCeY7UwgCryuvk3Wevm9ndMDvWAMjwGBfBJS12MqL1QoTQWEWt.json"
     );
-
-    pub static ref CONSENSUS_TEST_BLOCKS: HashMap<String, serde_json::Value> = load_json_test_blocks!(
-        "data/consensus/mainnet-113267-3NLenrog9wkiJMoA774T9VraqSUGhCuhbDLj3JKbEzomNdjr78G8.json"
-        "data/consensus/mainnet-77748-3NKaBJsN1SehD6iJwRwJSFmVzJg5DXSUQVgnMxtH4eer4aF5BrDK.json"
-    );
 }
 
 pub const VERIFICATION_KEY: &[u8] = include_bytes!(
