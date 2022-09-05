7aeb63e7898cc4b17d3239c276c3c1a2749f7b25
hanabi1224 <harlowmoo@gmail.com>
2022-09-05 14:39:24 +0800

    feat: impl GenesisProtocolState for qanet (#280)

--- consensus/src/genesis/genesis_impl.rs
+++ consensus/src/genesis/genesis_impl.rs
@@ -2,6 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use super::*;
+use mina_crypto::hash::*;
+use once_cell::sync::OnceCell;
+use proof_systems::mina_signer::CompressedPubKey;
+use std::str::FromStr;
 
 impl Genesis for ExternalTransition {
     /// Initialize genesis block
@@ -47,5 +51,161 @@ impl Genesis for ExternalTransition {
             proposed_protocol_version_opt: None,
             validation_callback: (),
         }
+    }
+}
+
+impl GenesisProtocolState<ProtocolState> for ProtocolState {
+    fn berkeley() -> &'static ProtocolState {
+        fn gen() -> anyhow::Result<ProtocolState> {
+            Ok( ProtocolState {
+                previous_state_hash: StateHash::from_str(
+                    "3NLUmnTBMCeExeWErijZ2GeLnjLtBgsDjN3qM8M8gcJDtk8k89xf",
+                )?,
+                body: ProtocolStateBody {
+                    genesis_state_hash: StateHash::from_str(
+                        "3NLUmnTBMCeExeWErijZ2GeLnjLtBgsDjN3qM8M8gcJDtk8k89xf",
+                    )?,
+                    blockchain_state: BlockchainState {
+                        staged_ledger_hash: StagedLedgerHash {
+                            non_snark: NonSnarkStagedLedgerHash {
+                                ledger_hash: LedgerHash::from_str(
+                                    "jwNYQU34Jb9FD6ZbKnWRALZqVDKbMrjZBKWFYZwAw8ZPMgv9Ld4",
+                                )?,
+                                aux_hash: AuxHash::from_str(
+                                    "UDRUFHSvxUAtV8sh7gzMVPqpbd46roG1wzWR6dYvB6RunPihom",
+                                )?,
+                                pending_coinbase_aux: PendingCoinbaseAuxHash::from_str(
+                                    "WAAeUjUnP9Q2JiabhJzJozcjiEmkZe8ob4cfFKSuq6pQSNmHh7",
+                                )?,
+                            },
+                            pending_coinbase_hash: CoinBaseHash::from_str(
+                                "2n27mUhCEctJbiZQdrk3kxYc7DVHvJVDErjXrjNs7jnP3HMLKtuN",
+                            )?,
+                        },
+                        genesis_ledger_hash: LedgerHash::from_str(
+                            "jwNYQU34Jb9FD6ZbKnWRALZqVDKbMrjZBKWFYZwAw8ZPMgv9Ld4",
+                        )?,
+                        registers: BlockchainStateRegisters {
+                            ledger: LedgerHash::from_str(
+                                "jwNYQU34Jb9FD6ZbKnWRALZqVDKbMrjZBKWFYZwAw8ZPMgv9Ld4",
+                            )?,
+                            pending_coinbase_stack: (),
+                            local_state:BlockchainStateRegistersLocalState {
+                                stack_frame: Field::from_str_radix(
+                                    "02F99BCFB0AA7F48C1888DA5A67196A2410FB084CD2DB1AF5216C5122AEBC054",
+                                    16,
+                                )?,
+                                call_stack: Field::from_str_radix(
+                                    "0000000000000000000000000000000000000000000000000000000000000000",
+                                    16,
+                                )?,
+                                transaction_commitment: Field::from_str_radix(
+                                    "0000000000000000000000000000000000000000000000000000000000000000",
+                                    16,
+                                )?,
+                                full_transaction_commitment: Field::from_str_radix(
+                                    "0000000000000000000000000000000000000000000000000000000000000000",
+                                    16,
+                                )?,
+                                token_id: TokenId(1),
+                                excess: SignedAmount(0, true),
+                                ledger: LedgerHash::from_str("jw6bz2wud1N6itRUHZ5ypo3267stk4UgzkiuWtAMPRZo9g4Udyd")?,
+                                success: true,
+                                party_index: MinaIndex(0),
+                                failure_status_tbl: Default::default(),
+                            },
+                        },
+                        timestamp: BlockTime(1655755201000),
+                        body_reference: BodyReference::from_hex(
+                            "36bda176656cc3be96c3d317db7b4ac06fdbc7f4eedcd6efdd20e28143d67421",
+                        )?,
+                    },
+                    consensus_state: ConsensusState {
+                        blockchain_length: 1.into(),
+                        epoch_count: 0.into(),
+                        min_window_density: 77.into(),
+                        sub_window_densities: vec![
+                            1.into(),
+                            7.into(),
+                            7.into(),
+                            7.into(),
+                            7.into(),
+                            7.into(),
+                            7.into(),
+                            7.into(),
+                            7.into(),
+                            7.into(),
+                            7.into(),
+                        ],
+                        last_vrf_output: VrfOutputTruncated::from_str(
+                            "OruOTtGM3tJL3jM0GHtCzKyugvWT0ZP7VckspHX8_g8=",
+                        )?,
+                        total_currency: 1013238001000001000.into(),
+                        curr_global_slot: GlobalSlot {
+                            slot_number: 0.into(),
+                            slots_per_epoch: 7140.into(),
+                        },
+                        global_slot_since_genesis: 0.into(),
+                        staking_epoch_data: EpochData {
+                            ledger: EpochLedger {
+                                hash: LedgerHash::from_str(
+                                    "jwNYQU34Jb9FD6ZbKnWRALZqVDKbMrjZBKWFYZwAw8ZPMgv9Ld4",
+                                )?,
+                                total_currency: 1013238001000001000.into(),
+                            },
+                            seed: EpochSeed::from_str(
+                                "2va9BGv9JrLTtrzZttiEMDYw1Zj6a6EHzXjmP9evHDTG3oEquURA",
+                            )?,
+                            start_checkpoint: StateHash::from_str(
+                                "3NK2tkzqqK5spR2sZ7tujjqPksL45M3UUrcA4WhCkeiPtnugyE2x",
+                            )?,
+                            lock_checkpoint: StateHash::from_str(
+                                "3NK2tkzqqK5spR2sZ7tujjqPksL45M3UUrcA4WhCkeiPtnugyE2x",
+                            )?,
+                            epoch_length: 1.into(),
+                        },
+                        next_epoch_data: EpochData {
+                            ledger: EpochLedger {
+                                hash: LedgerHash::from_str(
+                                    "jwNYQU34Jb9FD6ZbKnWRALZqVDKbMrjZBKWFYZwAw8ZPMgv9Ld4",
+                                )?,
+                                total_currency: 1013238001000001000.into(),
+                            },
+                            seed: EpochSeed::from_str(
+                                "2vc1zQHJx2xN72vaR4YDH31KwFSr5WHSEH2dzcfcq8jxBPcGiJJA",
+                            )?,
+                            start_checkpoint: StateHash::from_str(
+                                "3NK2tkzqqK5spR2sZ7tujjqPksL45M3UUrcA4WhCkeiPtnugyE2x",
+                            )?,
+                            lock_checkpoint: StateHash::from_str(
+                                "3NLUmnTBMCeExeWErijZ2GeLnjLtBgsDjN3qM8M8gcJDtk8k89xf",
+                            )?,
+                            epoch_length: 2.into(),
+                        },
+                        has_ancestor_in_same_checkpoint_window: true,
+                        block_stake_winner: CompressedPubKey::from_address(
+                            "B62qiy32p8kAKnny8ZFwoMhYpBppM1DWVCqAPBYNcXnsAHhnfAAuXgg",
+                        )?,
+                        block_creator: CompressedPubKey::from_address(
+                            "B62qiy32p8kAKnny8ZFwoMhYpBppM1DWVCqAPBYNcXnsAHhnfAAuXgg",
+                        )?,
+                        coinbase_receiver: CompressedPubKey::from_address(
+                            "B62qiy32p8kAKnny8ZFwoMhYpBppM1DWVCqAPBYNcXnsAHhnfAAuXgg",
+                        )?,
+                        supercharge_coinbase: true,
+                    },
+                    constants: ProtocolConstants {
+                        k: 290.into(),
+                        slots_per_epoch: 7140.into(),
+                        slots_per_sub_window: 7.into(),
+                        delta: 0.into(),
+                        genesis_state_timestamp: 1655755201000.into(),
+                    },
+                },
+            })
+        }
+
+        static INSTANCE: OnceCell<ProtocolState> = OnceCell::new();
+        INSTANCE.get_or_init(|| gen().expect("Failed to initialize berkeley protocol state"))
     }
 }
