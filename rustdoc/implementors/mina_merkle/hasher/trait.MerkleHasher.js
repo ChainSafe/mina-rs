7b74ab7c558819076a212126dddac81a30a9fd51
hanabi1224 <hanabi1224@users.noreply.github.com>
2022-09-21 05:34:58 +0000

    deploy: d2d6bbc2c5d6ade40d881a95222e57787e46a290

--- rustdoc/implementors/mina_merkle/hasher/trait.MerkleHasher.js
+++ rustdoc/implementors/mina_merkle/hasher/trait.MerkleHasher.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
-"mina_merkle":[]
+"mina_merkle":[],
+"mina_network":[["impl <a class=\"trait\" href=\"mina_merkle/hasher/trait.MerkleHasher.html\" title=\"trait mina_merkle::hasher::MerkleHasher\">MerkleHasher</a> for <a class=\"struct\" href=\"mina_network/processor/struct.DummyHasher.html\" title=\"struct mina_network::processor::DummyHasher\">DummyHasher</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
