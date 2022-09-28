1e101a1a4aca7f71273208b3c3d165744e8d9cc2
sudo-shashank <sudo-shashank@users.noreply.github.com>
2022-09-28 07:46:53 +0000

    deploy: 4f3328cdd24a880d99ab724241aba91c36a48eeb

--- rustdoc/implementors/serde/de/trait.Error.js
+++ rustdoc/implementors/serde/de/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"bin_prot":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.144/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"bin_prot/error/enum.Error.html\" title=\"enum bin_prot::error::Error\">Error</a>"]]
+"bin_prot":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.145/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"bin_prot/error/enum.Error.html\" title=\"enum bin_prot::error::Error\">Error</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- rustdoc/implementors/serde/ser/trait.Error.js
+++ rustdoc/implementors/serde/ser/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"bin_prot":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.144/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"bin_prot/error/enum.Error.html\" title=\"enum bin_prot::error::Error\">Error</a>"]]
+"bin_prot":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.145/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"bin_prot/error/enum.Error.html\" title=\"enum bin_prot::error::Error\">Error</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
